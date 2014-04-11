62820e71299bbb61562a870fa31f71153ff2a781
ey.engin.1991@googlemail.com <ey.engin.1991@googlemail.com@fb7b8e09-aaa3-eba5-c1d9-77c39db37fc8>
2014-04-11 20:25:31 +0000

    add empty HighScoreActivity

---
+++ trunk/app/src/main/java/com/aje/logic16/app/HighScoreActivity.java
@@ -0,0 +1,47 @@
+package com.aje.logic16.app;
+
+import android.database.DataSetObserver;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class HighScoreActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ListView HighscoreList = new ListView(this);
+
+        //HighscoreList.
+
+        setContentView(R.layout.activity_high_score);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.high_score, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
