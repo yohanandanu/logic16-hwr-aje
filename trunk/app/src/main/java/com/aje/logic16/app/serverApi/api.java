223aeadcf9eae8e581c5c8a70e3cdb91d7e5b55e
ey.engin.1991@googlemail.com <ey.engin.1991@googlemail.com@fb7b8e09-aaa3-eba5-c1d9-77c39db37fc8>
2014-04-14 12:19:14 +0000

    add highscoreListActivity + ArrayAdapter (actually not working) getHighscore check

--- trunk/app/src/main/java/com/aje/logic16/app/serverApi/api.java
+++ trunk/app/src/main/java/com/aje/logic16/app/serverApi/api.java
@@ -1,7 +1,11 @@
 package com.aje.logic16.app.serverApi;
 
+import android.accounts.NetworkErrorException;
+import android.os.AsyncTask;
+
 import com.aje.logic16.app.HighScoreLogic.highScore;
 
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
@@ -11,9 +15,11 @@ import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,6 +27,7 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 /**
  * Created by Engin on 09.04.14.
@@ -38,64 +45,25 @@ public class api {
     private static final String getHighScoreURL = hostUrl + "/" + getHighScoreFileName + "/" + getHighScoreParam;
     private static final String getFormURL = hostUrl + "/" + getFormFileName;
 
-    private HttpClient httpClient = new DefaultHttpClient();
+    //private HttpClient httpClient = new DefaultHttpClient();
 
 
     private int get(String url, ApiResponse out){
-
-        HttpGet request = new HttpGet(url);
-        request.addHeader("User-Agent", HTTP.USER_AGENT);
-
         try {
-            HttpResponse response = httpClient.execute(request);
-
-            System.out.println("\nSending 'GET' request to URL : " + url);
-            System.out.println("Response Code : " +
-                    response.getStatusLine().getStatusCode());
-
-            BufferedReader rd = new BufferedReader(
-                    new InputStreamReader(response.getEntity().getContent()));
-
-            StringBuffer result = new StringBuffer();
-            String line = "";
-            while ((line = rd.readLine()) != null) {
-                result.append(line);
-            }
-            if(response.getStatusLine().getStatusCode()== HttpStatus.SC_OK)
-            {
-                System.out.println("Response : " +
-                        result.toString());
-                out.response = result.toString();
-                return 1;
-            }
-            else
-               return response.getStatusLine().getStatusCode();
-
-        } catch (ClientProtocolException e) {
-        // TODO Auto-generated catch block
-        } catch (IOException e) {
-        // TODO Auto-generated catch block
+            out.response = new HttpTask().execute("GET",url).get();
+            return 1;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
         }
+
         return 0;
     }
 
     private int post(String URL, List<NameValuePair> Data){
         // Create a new HttpClient and Post Header
-        HttpPost httppost = new HttpPost(URL);
 
-        try {
-            // Add the given data
-            httppost.setEntity(new UrlEncodedFormEntity(Data, HTTP.UTF_8));
-            // Execute HTTP Post Request
-            HttpResponse response = this.httpClient.execute(httppost);
-            if(response.getStatusLine().getStatusCode()== HttpStatus.SC_OK)
-                 return 1;
-            else return response.getStatusLine().getStatusCode();
-        } catch (ClientProtocolException e) {
-            // TODO Auto-generated catch block
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-        }
         return 0;
     }
 
@@ -117,8 +85,8 @@ public class api {
         else return Integer.toString(res);
     }
 
-    private String getScore(int pos){
-        ApiResponse<String> response = new ApiResponse("");
+   private String getScore(int pos){
+            ApiResponse<String> response = new ApiResponse("");
 
         String highScoreUrl = getHighScoreURL + Integer.toString(pos);
         int res = get(highScoreUrl,response);
@@ -141,18 +109,14 @@ public class api {
 
             String[] responseSplit = response.split("#");
 
-            System.out.println("HighscoreResult : " +
-                    responseSplit[0] + " 1 " + responseSplit[1]);
-
-            if (responseSplit[0].length() > 0 || responseSplit[1].length() > 1)
+            if (responseSplit[0].length() <= 0 || responseSplit[1].length() <= 0)
                 throw new IllegalArgumentException();
 
             name = responseSplit[0];
-            score = Integer.getInteger(responseSplit[1]);
+            score = Integer.parseInt(responseSplit[1]);
+            return new highScore(name,score);
         }
         else throw new IllegalArgumentException();
-
-        return new highScore(name,score);
     }
 }
 
@@ -162,4 +126,82 @@ class ApiResponse<T> {
         this.response = value;
     }
     public String response;
+}
+
+
+class HttpTask extends AsyncTask<String,String,String>
+{
+
+    @Override
+    protected void onPostExecute(String result) {
+        // TODO Auto-generated method stub
+        super.onPostExecute(result);
+        // update textview here
+    }
+
+    @Override
+    protected void onPreExecute() {
+        // TODO Auto-generated method stub
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String methodParam = params[0];
+        String URL = params[1];
+
+
+
+        if (methodParam.contentEquals("GET"))
+        {
+            HttpGet requestGet = new HttpGet(URL);
+            requestGet.addHeader("User-Agent", HTTP.USER_AGENT);
+            return handleRequest(requestGet);
+        }
+        else if (methodParam.contentEquals("POST"))
+        {
+            String Data = (String) params[2];
+            HttpPost request = (HttpPost) new HttpPost(URL);
+            //request.setEntity(new UrlEncodedFormEntity(Data, HTTP.UTF_8));
+            return handleRequest(request);
+        }
+
+        throw new IllegalArgumentException();
+
+    }
+
+
+    private String handleRequest(HttpUriRequest request)
+    {
+        HttpClient httpclient = new DefaultHttpClient();
+
+        try
+        {
+            HttpResponse response = httpclient.execute(request);
+
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+            StringBuffer result = new StringBuffer();
+            String line = "";
+            while ((line = rd.readLine()) != null) {
+                result.append(line);
+            }
+            if(response.getStatusLine().getStatusCode()== HttpStatus.SC_OK){
+                System.out.println("Response : " +
+                        result.toString());
+                return result.toString();
+            } else
+            {
+                throw new NetworkErrorException();
+            }
+
+        } catch (ClientProtocolException e) {
+            e.getMessage();
+        } catch (IOException e) {
+            e.getMessage();
+        } catch (Exception e)
+        {
+            e.getMessage();
+        }
+        return "";
+    }
 }
